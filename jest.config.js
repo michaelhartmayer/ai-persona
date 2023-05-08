aa785d638217fb4747bbea221d4e46bf56ec818f
Michael Hartmayer <michaelhartmayer@gmail.com>
2023-05-07 23:32:23 -0700

    feat: more scaffolding

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts','tsx', 'js', 'jsx', 'json', 'node'],
+};
